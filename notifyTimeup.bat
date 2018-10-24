7d29830a41168fc9db298b6bac42406d686df830
nkiyo <nkiyo@hoge.com>
2018-10-24 09:14:35 +0900

    up

---
+++ notifyTimeup.bat
@@ -0,0 +1,5 @@
+rem powershell Set-ExecutionPolicy RemoteSigned
+cd C:\cygwin64\home\nkiyo\work\pomotimer
+powershell.exe -ExecutionPolicy bypass C:\cygwin64\home\nkiyo\work\pomotimer\notifyTimeup.ps1
+rem powershell Set-ExecutionPolicy Restricted
+
---
+++ notifyTimeup.ps1
@@ -0,0 +1,3 @@
+# Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+$wshell = New-Object -ComObject Wscript.Shell
+$wshell.Popup("Operation Completed",0,"Done",0x1)
